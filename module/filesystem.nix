e5c5c4025851d752a0a5b04ae483fe54d9012f7c
bibauporto <121832197+bibauporto@users.noreply.github.com>
2026-02-14 10:18:09 +0000

    Update filesystem.nix

--- module/filesystem.nix
+++ module/filesystem.nix
@@ -57,6 +57,18 @@
   # Swap (btrfs-safe)
   ##############################
 
+  # Enable zRAM swap
+  zramSwap.enable = true;
+  
+  # How much of your 16GB to dedicate to the zRAM device
+  # 50% to 100% is common; it only uses RAM when data is actually in it
+  zramSwap.memoryPercent = 70; 
+
+  # Optimization: Tell the kernel to prefer zRAM over the SSD swap
+  # By setting a higher priority (e.g., 100) than the SSD swap (usually -2)
+  zramSwap.priority = 100;
+
+
   swapDevices = [
     { device = "/persist/swap/swapfile"; }
   ];
