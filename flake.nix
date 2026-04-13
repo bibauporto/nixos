68cb737a9e85160fb6e2bbcd264168101d88ad79
bibauporto <pedroleal2651@gmail.com>
2026-04-13 11:54:33 +0000

    dd

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   inputs = {
+    # Primary: Small Unstable (faster CI/updates)
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    # Secondary: Full Unstable
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # Secondary: Stable
     # nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+
     impermanence.url = "github:nix-community/impermanence";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
+
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,18 +21,29 @@
 
   outputs =
     { nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
     {
       nixosConfigurations.lea-pc = nixpkgs.lib.nixosSystem {
+        inherit system;
         specialArgs = { inherit inputs; };
         modules = [
           {
-            nixpkgs.hostPlatform = "x86_64-linux";
             nixpkgs.config.allowUnfree = true;
             nixpkgs.overlays = [
-              inputs.nix-cachyos-kernel.overlays.pinned
+              # Optional: CachyOS overlay (commented out as in original)
+              # inputs.nix-cachyos-kernel.overlays.pinned
+
               # (final: prev: {
+              #   # Access via pkgs.stable
               #   stable = import inputs.nixpkgs-stable {
-              #     inherit (final) system;
+              #     inherit system;
+              #     config.allowUnfree = true;
+              #   };
+              #   # Access via pkgs.unstable (full branch)
+              #   unstable = import inputs.nixpkgs-unstable {
+              #     inherit system;
               #     config.allowUnfree = true;
               #   };
               # })
